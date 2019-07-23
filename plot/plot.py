b4caac1c8fbf84a72c8c6a60dd5c60a915e82e46
AB <4b11b4@gmail.com>
2019-07-22 20:06:07 -0700

    Make this script much nicer for the sake of getting back into Python... discover other github://kicad-3rd-party-tools

--- plot/plot.py
+++ plot/plot.py
@@ -12,12 +12,41 @@ def mkdir(name):
       raise
 
 
-# CONSTANTS 
-BOARD_LAYERS = 2
-DRILL_MAP = True
+# Setup
+## CONSTANTS 
+DRILL_MAP_EN = True #print drl-map pdf
+BOARD_NUM_LAYERS = 2 #2, 4...
+LAYERS = ["F_Cu",
+          #"F_Adhes",
+          "F_Paste",
+          "F_SilkS",
+          "F_Mask",
+          "B_Cu",
+          #"B_Adhes",
+          "B_Paste",
+          "B_SilkS",
+          "B_Mask",
+          #"Dwgs_User",
+          #"Cmts_User",
+          #"Eco1_User",
+          #"Eco2_User",
+          "Edge_Cuts",
+          #"Margin",
+          #"F_CrtYd",
+          #"F_Fab",
+          #"B_CrtYd",
+          #"B_Fab",
+         ]
+if BOARD_NUM_LAYERS == 4:
+  LAYERS.append("In1_Cu")
+  LAYERS.append("In2_Cu")
 
+## Might as well check if the layers are correct at the start
+print("Are you sure you want to export these layers?");
+for layer in LAYERS: print(layer)
+raw_input("[Warning] -- will overwrite files, press any key to continue:")
 
-# Start of script: full filename is passed as the only argument
+## Get only argument: full filename of .kicad_pcb file
 try:
   print("Loading file: %s" % sys.argv[1])
   fullname=sys.argv[1]
@@ -25,135 +54,74 @@ except:
   import glob
   fullname = glob.glob('./*.kicad_pcb')[0]
 
-without_extension = os.path.splitext(fullname)[0]
-# We removed extension, but not the name. Split the string at the slashes.
-split = without_extension.split('/')
-# Rejoin the string, but without the last split in the arr (and the first = '')
-join = '';
-for segment in split[1:len(split)-1]:
-    join += '/' + segment;
-path = join #The base path for exporting is the same as the .kicad_pcb file
-print('Exporting to: %s' % path);
+## Remove file extension and name to get base path
+without_extension = os.path.splitext(fullname)[0] #splitext vs split?
+path = os.path.abspath(os.path.split(fullname)[0]) #get rid of splitting...
+#The base path for exporting is at the .kicad_pcb file
 
-# We're outputting a lot of stuff... let's at least put it in one folder...
-exp_dir = path + '/fab'
-print('Base Export Directory: %s' % exp_dir)
+## Put everything in one folder
+exp_dir = path + '/fab';
+print('Exporting to: %s' % exp_dir);
 
-# Dictionary of export types
-exp_dict = { 'gerb': True,
-             'pdf' : True,
-             'dxf' : True,
-             'hpgl': True,
-             'post': True,
-             'svg' : True,
-             'und' : True,
-           }
-
-# Make directory for each export type
+## Dictionary of possible export types
+exp_dict = { 'dxf'   : 0,
+             'gerber': 1,
+             'hpgl'  : 0,
+             'pdf'   : 1,
+             'post'  : 1,
+             'svg'   : 1,
+             'undefined': 0 };
+## If enabled, make directory
+mkdir(exp_dir); # make the fab folder first
+DIRS = {}; # will contain path for each enabled output type
 for output, enabled in exp_dict.items():
-    print(output)
-    print(enabled)
-    mkdir(exp_dir)
     if(enabled):
-        mkdir(exp_dir + '/' + output + '/')
+        directory = exp_dir + '/' + output + '/'
+        DIRS.update({output:directory})
+        mkdir(directory)
+# End Setup
 
 
-# Load board
-brd = LoadBoard(filename)
+# PCBnew Objects 
+brd = LoadBoard(fullname) # Load board
+## http://
+pctl = PLOT_CONTROLLER(brd) # Create plotter
+## http://
+popt = pctl.GetPlotOptions() # Access plotter options
+## http://docs.kicad-pcb.org/doxygen/classPCB__PLOT__PARAMS.html
+drill = EXCELLON_WRITER(brd) # Create driller. Drill options set via driller.
+## http://docs.kicad-pcb.org/doxygen/classEXCELLON__WRITER.html
 
-# Create Excellon files
-drill = EXCELLON_WRITER(brd)
-#drill.SetOptions(mirror, minimalHeader, offset, mergeNPTH)
-drill.SetOptions(False, False, wxPoint(0,0), True)
-#drill.SetFormat(metric)
-drill.SetFormat(True)
-#drill.CreateDrillandMapFilesSet(location, generateDrill, generateMap)
-drill.CreateDrillandMapFilesSet(GERB_DIR, True, DRILL_MAP)
-####
-## NOTE: map is generated as a PDF by default?
-## ... manually move it from gerb folder? does peter use this? just don't generate it?
-## ... it is it usually generated as a gerber when going through the GUI?
-####
 
-# Create plotting object
-pctl = PLOT_CONTROLLER(brd)
-
-# Get current options of board?
-# what is the point if this if we set them below?
-popt = pctl.GetPlotOptions()
-
-# Gerber export 
-# Set plot options 
-# TODO: add rest of plot options from documentation
-popt.SetPlotFrameRef(False)
+# PCBnew Output
+## Settings (TODO: check fill before export?)
+drill.SetOptions(False, False, wxPoint(0,0), True) #mirror,min,offset,merge
+drill.SetFormat(True) # T=metric, F=imperial
+popt.SetAutoScale(False) #nodoc
+popt.SetExcludeEdgeLayer(True) #from other layers
 popt.SetLineWidth(FromMM(0.1))
-# TODO: what is auto scale? I dont see on GUI
-popt.SetAutoScale(False)
+popt.SetMirror(False) #diy boards
+popt.SetPlotFrameRef(False) #nodoc
+popt.SetPlotReference(False) #nodoc
+popt.SetPlotValue(False) #nodoc
 popt.SetScale(1)
-popt.SetMirror(False)
-popt.SetUseGerberAttributes(False)
-popt.SetExcludeEdgeLayer(True)
-popt.SetUseAuxOrigin(False)
-popt.SetPlotReference(False)
-popt.SetPlotValue(False)
-popt.SetSubtractMaskFromSilk(True)
-popt.SetUseGerberProtelExtensions(True)
-#popt.SetUseGerberX2Format(False)
+popt.SetSubtractMaskFromSilk(True) #erase silk if no mask underneath
+popt.SetUseAuxOrigin(False) #no additional origins are used
+popt.SetUseGerberAttributes(False) #dont use attributes
+popt.SetUseGerberProtelExtensions(True) #file extensions
 
-# TODO: investigate if possible to check for zone fills before exporting from python
-# TODO: plot values/ref options for popt?
-# TODO: subtract soldermask from silkscreen?
+## Generic Export (Iteration Over All Types)
+for output, dire in DIRS.items(): #'dir' is a keyword
+    popt.SetOutputDirectory(dire)
+    PLOT_FORMAT_TYPE = 'PLOT_FORMAT_' + output.upper()
+    print(PLOT_FORMAT_TYPE),
+    for layer in LAYERS:
+        print(layer),
+        pctl.SetLayer(eval(layer))
+        pctl.OpenPlotfile(layer, eval(PLOT_FORMAT_TYPE), "bbb")
+        pctl.PlotLayer()
+    pctl.ClosePlot()
+    print('')
 
-###
-# TODO: change this to be inside the manufacturing repository and additionally...
-# create flag to determine whether it ends up in a prototype folder or production folder
-# name the folder based on this flag, give increasing verison number based on previous exports, time & date, etc.
-###
-
-gerb_layers = ["F_Cu",
-               "F_SilkS",
-               #"F_Paste",
-               "F_Mask",
-               #"F_Fab",
-               "B_Cu",
-               "B_SilkS",
-               #"B_Paste",
-               "B_Mask",
-               #"B_Fab",
-               #"Eco2_User",
-               #"Cmts_User",
-               "Edge_Cuts"]
-
-# TODO catch for invalid BOARD_LAYERS, do nothing for 2 layers, etc.
-if BOARD_LAYERS == 4:
-  gerb_layers.append("In1_Cu")
-  gerb_layers.append("In2_Cu")
-
-print "exporting layers: " + str(gerb_layers)
-
-# Gerbers 
-popt.SetOutputDirectory(GERB_DIR)
-for name in gerb_layers:
-    pctl.SetLayer(eval(name))
-    # TODO: what does "bbb" mean below?
-    pctl.OpenPlotfile(name, PLOT_FORMAT_GERBER, "bbb")
-    pctl.PlotLayer()
-pctl.ClosePlot()
-
-# PDFs
-popt.SetOutputDirectory(PDF_DIR)
-for name in gerb_layers:
-    pctl.SetLayer(eval(name))
-    pctl.OpenPlotfile(name, PLOT_FORMAT_PDF, "bbb")
-    pctl.PlotLayer()
-pctl.ClosePlot()
-
-# PDFs
-popt.SetOutputDirectory(PDF_DIR)
-for name in gerb_layers:
-    pctl.SetLayer(eval(name))
-    pctl.OpenPlotfile(name, PLOT_FORMAT_PDF, "bbb")
-    pctl.PlotLayer()
-pctl.ClosePlot()
-
-# TODO: rename .gbr to .gts etc for Moko (aka Protel filenames)
+## Drill Export
+drill.CreateDrillandMapFilesSet(DIRS['gerber'], True, DRILL_MAP_EN) #loc,drl,map
